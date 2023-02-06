1e8a1de5665e185c6400e45146ef3400731acc34
Maydmor <leibetsederjakob@gmail.com>
2023-02-06 17:16:59 +0100

    implemented recomputation of property whenever any other property changes. Added new flag recompute_always to disable this behavior and use the old implementation

--- pydantic_computed/decorator.py
+++ pydantic_computed/decorator.py
@@ -1,11 +1,38 @@
-from pydantic import validator
+from pydantic import validator, root_validator
 from typing import Callable, Dict
+from inspect import getargspec, signature
 
-def computed(property_key: str):
+def computed(property_key: str, recompute_always: bool = True):
     def __computed_wrapper(method: Callable):
-        def __method_wrapper(cls, v, values: Dict):
-            """pydantics "validator" wrapper calls the given method with cls, v, and the dict containing the models values,
-            for ease of use get rid of cls and v and simply pass the dict of values to the wrapped function"""
+        def __compute_once_method_wrapper(cls, v, values: Dict):
+            """
+            this wrapper will only be called when recompute_always is set to False. It can be used to save computation time, since it only 
+            computes the value when the pydantic object is instantiated.
+
+            pydantics "validator" wrapper calls the given method with cls, v, and the dict containing the models values,
+            for ease of use get rid of cls and v and simply pass the dict of values to the wrapped function.
+            """
             return method(**values)
-        return validator(property_key, pre=True, always=True, allow_reuse=True)(__method_wrapper)
+        def __method_wrapper(cls, values):
+            """ This wrapper is the default method that gets called. It is called whenever any property in the model changes and therefore
+            always recomputes the property.
+            """
+            cls.Config.validate_assignment = True # Rerun code on every assignment operation of any property
+            method_params = signature(method).parameters
+            # create the dictionary containing all named parameters of the compute method e.g. def calculate_c(a: int, b: int, **kwargs) -> {a, b} 
+            parameter_names = [name for name,param in method_params.items() if param.kind == param.POSITIONAL_OR_KEYWORD]
+            parameters = {property: values[property] for property in parameter_names}
+            # create the dictionary containing all remaining unnamed/kwarg parameters
+            new_kwarg_names = list(filter(lambda key: key not in parameters, values.keys()))
+            new_kwargs = {property: values[property] for property in new_kwarg_names if property != property_key}
+            # pass in var keyword arguments if function has e.g. **kwargs in signature
+            contains_var_keyword_arg = lambda params: len(list(filter(lambda param: param.kind == param.VAR_KEYWORD, params))) > 0
+            method_args = dict(new_kwargs, **parameters) if contains_var_keyword_arg(method_params.values()) else parameters
+            # overwrite value of computed property with method output
+            values[property_key] = method(**method_args)
+            return values
+        if recompute_always:
+            return root_validator(allow_reuse=True, pre=True)(__method_wrapper)
+        else:
+            return validator(property_key, pre=True, allow_reuse=True, always=True)(__compute_once_method_wrapper)
     return __computed_wrapper
--- tests/test_computed.py
+++ tests/test_computed.py
@@ -8,6 +8,7 @@ class ComputedModelInt(BaseModel):
     c: Computed[int]
     @computed('c')
     def calculate_c(a: int, b: int):
+        print(f'calculate c {a}, {b}')
         return a + b
 
 class ComputedModelStr(BaseModel):
@@ -15,9 +16,25 @@ class ComputedModelStr(BaseModel):
     b: int
     c: Computed[str]
     @computed('c')
-    def calculate_c(a: int, b: int):
-        return a + b
-    
+    def calculate_c(a: int, b: int, **kwargs)->str:
+        return str(a + b)
+
+class ComputedModelKwargs(BaseModel):
+    a: int
+    b: int 
+    c: Computed[str]
+    @computed('c')
+    def calculate_c(a:int, **kwargs):
+        return kwargs.get('b') * a
+
+class ComputedModelNoArgs(BaseModel):
+    a: int
+    b: int
+    c: Computed[int]
+    @computed('c')
+    def calculate_c():
+        return 5
+
 def test_simple():
     model = ComputedModelInt(a=1, b=2)
     assert model.c == 3
@@ -37,4 +54,18 @@ def test_overwrites_value():
 def test_datatype_conversion():
     model = ComputedModelStr(a=1, b=2)
     assert model.c == '3'
-    
+
+def test_property_change():
+    model = ComputedModelInt(a = 1, b = 2)
+    model.c = 3
+    model.a = 2
+    model.b = 3
+    assert model.c == 5
+
+def test_use_kwargs():
+    model = ComputedModelKwargs(a=2, b=3)
+    assert model.c == '6'
+
+def test_no_parameters():
+    model = ComputedModelNoArgs(a = 1, b = 2)
+    assert model.c == 5
